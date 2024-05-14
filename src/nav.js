2a78d63a230dd7277ac45f32e6718867ab208d92
pycogram <kaluifesinachi@yahoo.com>
2024-05-14 18:33:03 +0100

    navbar added

--- src/nav.js
+++ src/nav.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
 export default function Navbar( props ){
 
     const justImg = props.logo1;
+
+    let [ isMenuActive, setMenuActive ] = useState(false);
+    const [menuButton, setMenuButton] = useState("menuBtn");
+    const [menuOption, setMenuOption] = useState("menuOptionInactive");
+
+    const MenuActive = () => {
+        if(!isMenuActive){
+            setMenuButton("menuBtn menuBtnActive");
+            setMenuOption("nav menuOptionActive")
+        } else {
+            setMenuButton("menuBtn");
+            setMenuOption("menuOptionInactive")
+        }
+        setMenuActive(!isMenuActive);
+    }
+    const closeOption = () => {
+        if(!isMenuActive){
+            setMenuButton("menuBtn menuBtnActive");
+            setMenuOption("nav menuOptionActive")
+        } else {
+            setMenuButton("menuBtn");
+            setMenuOption("menuOptionInactive")
+        }
+        setMenuActive(!isMenuActive);
+    }
     return(
         <div className="homePage">
             <nav>
                 <img src={justImg} alt="Pepe Rocky Logo"></img>
-                <ul>
-                    <Link to={"/home"}><li><h3>Home</h3></li></Link>
-                    <Link to={"/about"}><li><h3>About</h3></li></Link>
-                    <Link to={"/tokeno"}><li><h3>Tokenomics</h3></li></Link>
-                    <Link to={"/rmap"}><li><h3>Roadmap</h3></li></Link>
-                    <Link to={"/contact"}><li><h3>Contact</h3></li></Link> 
+                <ul className={menuOption}>
+                    <Link to={"/home"} onClick={() => closeOption()} ><li><h3>Home</h3></li></Link>
+                    <Link to={"/about"} onClick={() => closeOption()}><li><h3>About</h3></li></Link>
+                    <Link to={"/tokeno"} onClick={() => closeOption()}><li><h3>Tokenomics</h3></li></Link>
+                    <Link to={"/rmap"} onClick={() => closeOption()}><li><h3>Roadmap</h3></li></Link>
+                    <Link to={"/contact"} onClick={() => closeOption()}><li><h3>Contact</h3></li></Link> 
                 </ul>
                 <button>Buy $PEPER</button>
-                <span className="menuBtn">
-
-                </span>
+                <span className={menuButton}  onClick={() => MenuActive()}></span>
             </nav>
         </div>
     )
